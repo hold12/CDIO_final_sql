ed2f07bc12dfa32d0ff6e94eba00509c5ec2ff81
Freya Helstrup <freyagh@me.com>
2017-04-18 10:49:09 +0200

    UpdateOperator can now change all fields at once

--- updateOperator.sql
+++ updateOperator.sql
@@ -4,20 +4,85 @@ DROP PROCEDURE IF EXISTS updateOperator;
 
 DELIMITER //
 
-CREATE PROCEDURE updateOperator (
-  IN id INT(11),
-  IN fieldToUpdate TEXT,
-  IN newValue TEXT
+CREATE PROCEDURE updateOperator(
+  IN id         INT(11),
+  IN first_name TEXT,
+  IN last_name  TEXT,
+  IN initials   TEXT,
+  IN cpr        TEXT,
+  IN password   TEXT
 )
-BEGIN
-  SET @s = CONCAT('
-  UPDATE operator
-  SET ',fieldToUpdate,' = ''',newValue,'''
-  WHERE operator_id = ''',id,''';');
-  PREPARE statement FROM @s;
-  EXECUTE statement;
-  DEALLOCATE PREPARE statement;
+  BEGIN
+    IF first_name != ''
+    THEN
+      BEGIN
+        SET @s = CONCAT('
+          UPDATE operator
+          SET operator_firstname = ''', first_name, '''
+          WHERE operator_id = ''', id, ''';
+        ');
+        PREPARE statement FROM @s;
+        EXECUTE statement;
+        DEALLOCATE PREPARE statement;
+      END;
+    END IF;
 
-END//
+    IF last_name != ''
+    THEN
+      BEGIN
+        SET @s = CONCAT('
+          UPDATE operator
+          SET operator_lastname = ''', last_name, '''
+          WHERE operator_id = ''', id, ''';
+        ');
+        PREPARE statement FROM @s;
+        EXECUTE statement;
+        DEALLOCATE PREPARE statement;
+      END;
+    END IF;
+
+    IF initials != ''
+    THEN
+      BEGIN
+        SET @s = CONCAT('
+          UPDATE operator
+          SET initials = ''', initials, '''
+          WHERE operator_id = ''', id, ''';
+        ');
+        PREPARE statement FROM @s;
+        EXECUTE statement;
+        DEALLOCATE PREPARE statement;
+      END;
+    END IF;
+
+    IF cpr != ''
+    THEN
+      BEGIN
+        SET @s = CONCAT('
+          UPDATE operator
+          SET cpr = ''', cpr, '''
+          WHERE operator_id = ''', id, ''';
+        ');
+        PREPARE statement FROM @s;
+        EXECUTE statement;
+        DEALLOCATE PREPARE statement;
+      END;
+    END IF;
+
+    IF password != ''
+    THEN
+      BEGIN
+        SET @s = CONCAT('
+          UPDATE operator
+          SET password = ''', password, '''
+          WHERE operator_id = ''', id, ''';
+        ');
+        PREPARE statement FROM @s;
+        EXECUTE statement;
+        DEALLOCATE PREPARE statement;
+      END;
+    END IF;
+
+  END//
 
 DELIMITER ;
