65f9e1e422f68af762088c1e29e3fbe4042216a4
Freya Helstrup <freyagh@me.com>
2017-06-05 16:44:55 +0200

    Added role insertion to insertUser

--- insert_procedures/insertUser.sql
+++ insert_procedures/insertUser.sql
@@ -4,14 +4,19 @@ CREATE PROCEDURE insertUser(
   IN user_firstname TEXT,
   IN user_lastname  TEXT,
   IN initials       TEXT,
-  IN password       TEXT
+  IN password       TEXT,
+  IN roles          TEXT
 )
   BEGIN
-    SET @user_id_v = (SELECT IFNULL(MAX(user_id), 0) + 1 FROM user);
+    START TRANSACTION;
+    SET @user_id_v = (SELECT IFNULL(MAX(user_id), 0) + 1
+                      FROM user);
     SET @user_firstname_v = user_firstname;
     SET @user_lastname_v = user_lastname;
     SET @initials_v = initials;
     SET @password_v = password;
+    SET @roles_v = roles;
+
     SET @s = 'INSERT INTO user(user_id, user_firstname, user_lastname, initials, password, is_active) VALUES(?,?,?,?,?,1);';
 
     PREPARE statement FROM @s;
@@ -20,5 +25,18 @@ CREATE PROCEDURE insertUser(
     DEALLOCATE PREPARE statement;
 
     SELECT @user_id_v AS user_id;
+
+    SET @s = CONCAT('INSERT INTO user_role(user_id, role_id)
+    SELECT DISTINCT user.user_id, role.role_id
+    FROM user
+    CROSS JOIN role
+    WHERE user_id = ?
+      AND role_id IN (',@roles_v,')');
+
+    PREPARE statement FROM @s;
+    EXECUTE statement USING @user_id_v;
+    DEALLOCATE PREPARE statement;
+
+    COMMIT;
   END//
 DELIMITER ;
