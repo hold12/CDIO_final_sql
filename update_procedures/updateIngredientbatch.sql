43e49c50f23592cda70b947b4d71afbd8b393036
Freya Helstrup <freyagh@me.com>
2017-06-08 13:30:16 +0200

    Update of ID's in updateIngredientBatch + updateProductbatch

--- update_procedures/updateIngredientbatch.sql
+++ update_procedures/updateIngredientbatch.sql
@@ -1,21 +1,22 @@
 DROP PROCEDURE IF EXISTS updateIngredientbatch;
 DELIMITER //
 CREATE PROCEDURE updateIngredientbatch(
-  IN id            INT(11),
-  IN ingredient_id INT(11),
-  IN amount        DOUBLE
+  IN ingredientbatch_id INT(11),
+  IN ingredient_id      INT(11),
+  IN amount             DOUBLE
 )
   BEGIN
-    SET @ingredientbatch_id = id;
+    SET @ingredientbatch_id_v = ingredientbatch_id;
     SET @ingredient_id_v = ingredient_id;
     SET @amount_v = amount;
     SET @s = 'UPDATE ingredientbatch
       SET ingredient_id = ?
         , amount = ?
       WHERE ingredientbatch_id = ?;';
-    
-	PREPARE statement FROM @s;
-    EXECUTE statement USING @ingredient_id_v, @amount_v, @ingredientbatch_id;
+
+    PREPARE statement FROM @s;
+    EXECUTE statement
+    USING @ingredient_id_v, @amount_v, @ingredientbatch_id_v;
     DEALLOCATE PREPARE statement;
   END//
 DELIMITER ;
--- update_procedures/updateProductbatch.sql
+++ update_procedures/updateProductbatch.sql
@@ -1,13 +1,13 @@
 DROP PROCEDURE IF EXISTS updateProductbatch;
 DELIMITER //
 CREATE PROCEDURE updateProductbatch(
-  IN id        INT(11),
-  IN status    INT(11),
-  IN recipe_id INT(11),
-  IN user_id   INT(11)
+  IN productbatch_id INT(11),
+  IN status          INT(11),
+  IN recipe_id       INT(11),
+  IN user_id         INT(11)
 )
   BEGIN
-    SET @productbatch_id = id;
+    SET @productbatch_id_v = productbatch_id;
     SET @status_v = status;
     SET @recipe_id_v = recipe_id;
     SET @user_id_v = user_id;
@@ -18,7 +18,8 @@ CREATE PROCEDURE updateProductbatch(
       WHERE productbatch_id = ?';
 
     PREPARE statement FROM @s;
-    EXECUTE statement USING @status_v, @recipe_id_v, @user_id_v, @productbatch_id;
+    EXECUTE statement
+    USING @status_v, @recipe_id_v, @user_id_v, @productbatch_id_v;
     DEALLOCATE PREPARE statement;
   END//
 DELIMITER ;
