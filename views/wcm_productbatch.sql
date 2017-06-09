f659e4d49a64a2c70b21d6d3289700022e9a6fa8
Freya Helstrup <freyagh@me.com>
2017-06-09 13:56:45 +0200

    Updated WCM productbatch view to include created_time

--- views/wcm_productbatch.sql
+++ views/wcm_productbatch.sql
@@ -1,6 +1,7 @@
 DROP VIEW IF EXISTS wcm_productbatch;
-CREATE VIEW wcm_productbatch(productbatch_id, finished_time, status, recipe_id, user_id) AS
+CREATE VIEW wcm_productbatch(productbatch_id, created_time, finished_time, status, recipe_id, user_id) AS
   SELECT productbatch_id,
+    created_time,
 	finished_time, 
 	status, 
 	recipe_id, 
