cd6737ac1eb488b43966af695a62122269cbeb50
Freya Helstrup <freyagh@me.com>
2017-06-15 09:38:57 +0200

    Updated columns of web_role_permission

--- views/web_role_permission.sql
+++ views/web_role_permission.sql
@@ -1,5 +1,6 @@
 DROP VIEW IF EXISTS web_role_permission;
-CREATE VIEW web_role_permission(recipe_id, ingredient_id, nominated_net_weight, tolerance) AS
-  SELECT permission_name,role_name FROM permission
+CREATE VIEW web_role_permission(permission_name, role_name) AS
+  SELECT permission_name, role_name 
+  FROM permission
   JOIN role_permission ON permission.permission_id = role_permission.permission_id
   JOIN role ON role_permission.role_id = role.role_id;
